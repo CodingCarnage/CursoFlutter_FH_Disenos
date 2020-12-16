b0b2b4167f79e2f013d2da1c1acf8642349a1cb5
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-15 22:17:11 -0600

    Correciones

--- lib/src/pages/menu_page.dart
+++ lib/src/pages/menu_page.dart
@@ -30,14 +30,15 @@ class MenuPage extends StatelessWidget {
     final gradient = Container(
       width: double.infinity,
       height: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            begin: FractionalOffset(0.0, 0.6),
-            end: FractionalOffset(0.0, 1.0),
-            colors: [
-              Color.fromRGBO(52, 54, 101, 1.0),
-              Color.fromRGBO(35, 37, 57, 1.0)
-            ]),
+      decoration: const BoxDecoration(
+        gradient: const LinearGradient(
+          begin: const FractionalOffset(0.0, 0.6),
+          end: const FractionalOffset(0.0, 1.0),
+          colors: const <Color>[
+            const Color.fromRGBO(52, 54, 101, 1.0),
+            const Color.fromRGBO(35, 37, 57, 1.0)
+          ],
+        ),
       ),
     );
 
@@ -47,10 +48,12 @@ class MenuPage extends StatelessWidget {
         height: 360.0,
         width: 360.0,
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromRGBO(236, 98, 188, 1.0),
-            Color.fromRGBO(241, 142, 172, 1.0),
-          ]),
+          gradient: const LinearGradient(
+            colors: const <Color>[
+              const Color.fromRGBO(236, 98, 188, 1.0),
+              const Color.fromRGBO(241, 142, 172, 1.0),
+            ],
+          ),
           borderRadius: BorderRadius.circular(80.0),
         ),
       ),
@@ -74,20 +77,18 @@ class MenuPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
+            const Text(
               'Classify transaction',
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 30.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
+            const SizedBox(height: 10.0),
+            const Text(
               'Classify this transaction into a particular category',
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 18.0,
               ),
@@ -101,18 +102,23 @@ class MenuPage extends StatelessWidget {
   Widget _bottomNavigationBar(BuildContext context) {
     return Theme(
       data: Theme.of(context).copyWith(
-          canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+          canvasColor: const Color.fromRGBO(55, 57, 84, 1.0),
           primaryColor: Colors.pinkAccent,
           textTheme: Theme.of(context).textTheme.copyWith(
-              caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))),
+                caption: const TextStyle(
+                  color: const Color.fromRGBO(116, 117, 152, 1.0),
+                ),
+              )),
       child: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
         items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: ''),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.supervised_user_circle), label: ''),
+          const BottomNavigationBarItem(
+              icon: const Icon(Icons.calendar_today), label: ''),
+          const BottomNavigationBarItem(
+              icon: const Icon(Icons.bar_chart), label: ''),
+          const BottomNavigationBarItem(
+              icon: const Icon(Icons.supervised_user_circle), label: ''),
         ],
       ),
     );
@@ -146,20 +152,23 @@ class MenuPage extends StatelessWidget {
       opacity: 0.80,
       child: Container(
         height: 180.0,
-        margin: EdgeInsets.all(15.0),
+        margin: const EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20.0),
-          color: Color.fromRGBO(62, 66, 107, 1.0),
+          color: const Color.fromRGBO(62, 66, 107, 1.0),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
+          children: <Widget>[
             CircleAvatar(
               backgroundColor: color,
               radius: 30.0,
               child: Icon(icon, color: Colors.white, size: 30.0),
             ),
-            Text(text, style: TextStyle(color: color)),
+            Text(
+              text,
+              style: TextStyle(color: color),
+            ),
           ],
         ),
       ),
